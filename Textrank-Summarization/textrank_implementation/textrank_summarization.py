afeb7dbae1b22cf3c81f2eb04db55422245d2955
Georgi Vankov <georgi.vankov@lights.digital>
2021-12-30 17:28:53 +0200

    Add textrank implementation

---
+++ Textrank-Summarization/textrank_implementation/textrank_summarization.py
@@ -0,0 +1,142 @@
+import os
+import numpy as np
+import pandas as pd
+import networkx as nx
+import multiprocessing
+from nltk.tokenize import sent_tokenize
+from stop_words import safe_get_stop_words
+from gensim.models import Word2Vec
+from scipy import spatial
+from rouge import Rouge
+
+from xml_parser import XMLParser
+
+class TextRankSummarizator():
+    def __init__(self, language):
+        self.stop_words = safe_get_stop_words(language)
+        # self.w2v_model = {}
+        
+        # Use pretrained model for english
+        # self.load_pretrained_word2vec_model()
+
+    def set_filepath(self, filePath):
+        self.filePath = filePath
+
+    def load_pretrained_word2vec_model(self):
+        f = open('./glove/glove.6B.100d.txt', encoding='utf-8')
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            self.w2v_model[word] = coefs
+        f.close()
+
+    def load_sentences(self):
+        if self.filePath.endswith('.xml'):
+            parser = XMLParser()
+            data = parser.read(self.filePath)
+        else:
+            f = open(self.filePath, "r", encoding="utf8")
+            data= f.read()
+
+        sentences = sent_tokenize(data)
+        return sentences
+
+    def sentence_normalization(self, sentences):
+        # remove punctuations
+        clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Zа-яА-Я]", " ")
+
+        # remove uppercase
+        clean_sentences = [s.lower() for s in clean_sentences]
+
+        #remove stopwords
+        clean_sentences = [self.remove_stopwords(r.split()) for r in clean_sentences]
+
+        return clean_sentences
+
+    def remove_stopwords(self, sentence):
+        newSentence = " ".join([i for i in sentence if i not in self.stop_words])
+        return newSentence
+
+    def sentence_embeddings(self, sentences):
+        # Extract word vectors
+        cores = multiprocessing.cpu_count()
+        w2v_model = Word2Vec(size=10,
+                            min_count=1,
+                            workers=cores-1)
+        w2v_model.build_vocab(sentences)
+
+        total_examples = w2v_model.corpus_count
+        w2v_model.train(sentences, total_examples=total_examples, epochs=w2v_model.epochs)
+        sentence_embeddings=[[sum(w2v_model.wv[word]) for word in words] for words in sentences]
+        max_len=max([len(tokens) for tokens in sentences])
+        sentence_embeddings=[np.pad(embedding,(0,max_len-len(embedding)),'constant') for embedding in sentence_embeddings]
+        
+        return sentence_embeddings
+
+    def build_similarity_matrix(self, normalized_sentences, sentence_embeddings):
+        print(len(normalized_sentences))
+        similarity_matrix = np.zeros([len(normalized_sentences), len(normalized_sentences)])
+        for i,row_embedding in enumerate(sentence_embeddings):
+            for j,column_embedding in enumerate(sentence_embeddings):
+                if similarity_matrix[i][j] == 0:
+                    similarity_matrix[j][i] = similarity_matrix[i][j] = 1 - spatial.distance.cosine(row_embedding,column_embedding)
+        similarity_matrix[np.isnan(similarity_matrix)] = 0
+        return similarity_matrix
+
+    def rank_sentences(self, similarity_matrix, raw_sentences):
+        nx_graph = nx.from_numpy_array(similarity_matrix)
+        scores = nx.pagerank_numpy(nx_graph)
+
+        ranked_sentences = sorted(((scores[i],s, i) for i,s in enumerate(raw_sentences)), reverse=True)
+
+        return ranked_sentences
+
+    def construct_summary(self, ranked_sentences, number_of_sentences):
+        selected_sentences = ranked_sentences[:number_of_sentences]
+        selected_sentences.sort(key=lambda a: a[2])
+        summary = ''
+        for s in selected_sentences:
+            summary += s[1] + '\n'#(str(s[0]) + ' -> ' + str(s[2]) + '\n')
+        
+        return summary        
+
+    #"tennis.csv"
+    #"cloud_tech.txt"
+    def get_summary(self, len):
+        raw_sentences = self.load_sentences()
+        normalized_sentences = self.sentence_normalization(raw_sentences)
+        sentence_embeddings = self.sentence_embeddings(normalized_sentences)
+        similarity_matrix = self.build_similarity_matrix(normalized_sentences, sentence_embeddings)
+        ranked_sentences = self.rank_sentences(similarity_matrix, raw_sentences)
+        summary = self.construct_summary(ranked_sentences, len)
+
+        return summary
+
+
+# s = TextRankSummarizator("bulgarian", './top1000_complete/A00-1031/Documents_xml/A00-1031.xml')
+# print(s.get_summary(5))
+
+def evaluate():
+    original_summaries = []
+    generated_summaries = []
+    summarizator = TextRankSummarizator('english') 
+    for index, filename in enumerate(os.listdir("./top1000_complete")):
+        if index == 50: break
+        print(index, filename)
+        summarizator.set_filepath('./top1000_complete/{0}/Documents_xml/{0}.xml'.format(filename))
+        f = open('./top1000_complete/{0}/summary/{0}.gold.txt'.format(filename), "r", encoding="utf8")
+        # try:
+        original_summaries.append(f.read())
+        summary = summarizator.get_summary(original_summaries[-1].count('\n')+5)
+
+        generated_summaries.append(summary)
+
+
+    rouge = Rouge()
+    print(rouge.get_scores(original_summaries, generated_summaries, avg=True))
+    print('\n')
+
+evaluate()
+
+
